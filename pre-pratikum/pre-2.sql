ff98aed83ecf7cfb5994977622d4fa418c9f26e1
yogi-saputr <145946382+yogi-saputr@users.noreply.github.com>
2025-04-14 10:27:53 +0800

    Add files via upload

---
+++ pre-pratikum/pre-2.sql
@@ -0,0 +1,18 @@
+-- Nama : Yogi Saputra
+-- NIM : 23241007
+-- Kelas : A
+
+-- Melihat Database dalam MySQl
+SHOW DATABASES;
+
+-- Memilih database yang digunakan
+USE mysql;
+
+-- Menampilkan table dalam database mysql
+SHOW TABLES;
+
+-- Mendeskripsikan tabel user
+DESCRIBE user;
+
+-- Menampilkan isi data dari kolom Host, User, Password
+SELECT Host, User, Password FROM user;
